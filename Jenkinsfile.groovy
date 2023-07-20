8abd18d1c682d3be347e463ebea3a779f23d4a90
Moufadal <79516540+moufadal-Goulamhoussen974@users.noreply.github.com>
2023-07-20 13:33:03 +0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,18 +1,19 @@
 pipeline {
     agent any
     triggers {
-        cron('H/5 * * * *') 
+        pollSCM('H/5 * * * *') 
     }
     stages {
         stage('Git Clone') {
             steps {
                 git branch: 'main',  url: 'https://github.com/moufadal-Goulamhoussen974/TPDevOps.git'
             }
-              stage('Run Tests') {
+             
+    }
+         stage('Run Tests') {
             steps {
                 sh 'cat /var/lib/jenkins/workspace/Test/Test.html | grep "Ceci est un paragraphe de texte."' 
             }
         }
-    }
 }
 }
